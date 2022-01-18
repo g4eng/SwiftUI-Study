ac8b7a4d0c5f8684b2ac8febab4d1f3f3d421bca
g4eng <gaeng1107@gmail.com>
2022-01-18 20:06:32 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by gaeng on 2022/01/18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
