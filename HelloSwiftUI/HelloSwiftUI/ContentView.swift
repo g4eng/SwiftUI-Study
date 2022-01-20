3eb48e60093b399e59844d0c05b17272fa6cae3d
g4eng <gaeng1107@gmail.com>
2022-01-20 23:45:20 +0900

    22 01 20

---
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI
+//
+//  Created by gaeng on 2022/01/19.
+//
+
+import SwiftUI
+
+// VC와 같은 역할 
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloSwiftUI/HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by gaeng on 2022/01/19.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
