acb23f63695edd38b687e5728591bbb59252c673
양경모 <gaeng2y@gmail.com>
2022-09-14 21:28:53 +0900

    03. ProductRow 만들기

--- FruitMart/FruitMart/Source/User Interface/Home/ProductRow.swift
+++ FruitMart/FruitMart/Source/User Interface/Home/ProductRow.swift
@@ -10,7 +10,62 @@ import SwiftUI
 
 struct ProductRow: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            productImage
+            productDescription
+        }
+        .frame(height: 150)
+        .background(Color.primary.colorInvert())
+        .cornerRadius(6)
+        .shadow(color: Color.primary.opacity(0.33), radius: 1, x: 2, y: 2)
+        .padding(.vertical, 8)
+    }
+}
+
+private extension ProductRow {
+    var productImage: some View {
+        Image("apple")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 140)
+            .clipped()
+    }
+    
+    var productDescription: some View {
+        VStack(alignment: .leading) {
+            // 상품명 부분에 작성
+            Text("백설공주 사과")
+                .font(.headline)
+                .fontWeight(.medium)
+                .padding(.bottom, 6)
+            // 상품 설명 부분에 작성
+            Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+            Spacer()
+            footerView
+        }
+        .padding([.leading, .bottom], 12)
+        .padding([.top, .trailing])
+    }
+    
+    var footerView: some View {
+        HStack(spacing: 0) {
+            Text("₩").font(.footnote)
+            + Text("2100").font(.headline)
+            
+            Spacer()
+            
+            Image(systemName: "heart")
+                .imageScale(.large)
+                .foregroundColor(Color("peach"))
+                .frame(width: 32, height: 32)
+            
+            Image(systemName: "cart")
+                .imageScale(.large)
+                .foregroundColor(Color("peach"))
+                .frame(width: 32, height: 32)
+        }
     }
 }
 
