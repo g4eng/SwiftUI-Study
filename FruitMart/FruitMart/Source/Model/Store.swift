c031dcd2e25838ae2baafeb0c5e2b4a3c82dbf73
양경모 <gaeng2y@gmail.com>
2022-09-17 00:40:35 +0900

    7. 리스트를 이용해 상품 목록 표시하기

---
+++ FruitMart/FruitMart/Source/Model/Store.swift
@@ -0,0 +1,16 @@
+//
+//  Store.swift
+//  FruitMart
+//
+//  Created by gaeng on 2022/09/17.
+//
+
+import Foundation
+
+final class Store {
+    var products: [Product]
+    
+    init(filename: String = "ProductData.json") {
+        self.products = Bundle.main.decode(filename: filename, as: [Product].self)
+    }
+}
--- FruitMart/FruitMart/Source/User Interface/Home/Home.swift
+++ FruitMart/FruitMart/Source/User Interface/Home/Home.swift
@@ -9,33 +9,19 @@
 import SwiftUI
 
 struct Home: View {
+    let store: Store
+    
   var body: some View {
-      /*
-       구조
-       HStack {
-           // 상품 이미지
-           VStack {
-               // 상품명
-               // 상품 설명
-               HStack {
-                   // 가격 정보와 버튼
-                   EmptyView() // 1. 어떤 내용도 표현하지 않으며 공간도 차지하지 않는 뷰, 스택 내부에는 반드시 하나 이상의 뷰를 넣어야 하므로, 이후에 추가할 기능을 구현하기 전에 임시방편으로 넣어둠
-               }
-           }
-       }
-       .frame(height: 150)
-       */
-      VStack {
-          ProductRow(product: productSamples[0])
-          ProductRow(product: productSamples[1])
-          ProductRow(product: productSamples[2])
+      List(store.products) { product in
+          ProductRow(product: product)
       }
   }
 }
 
 struct Home_Previews: PreviewProvider {
   static var previews: some View {
-      Home()
+      Home(store: Store())
           .preferredColorScheme(.light)
   }
 }
+
